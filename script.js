7a3caf29db74b79cd332cec5d12b1d5e847b0a3b
kesia <kesia.alves@ymail.com>
2023-10-04 11:46:39 -0300

    segunda aula de javascript

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-document.write("Olá,mundo!");
-console.log("Imprimindo no console!");
-window.alert("Texto no modal do navegador!");
+//document.write("Olá,mundo!");
+//console.log("Imprimindo no console!");
+//window.alert("Texto no modal do navegador!");
+
+//logica de programacao
+//problema : receber 3 notas de um aluno,
+//calcular e apresentar a media e o resultado
+//se o aluno foi aprovado ou não
+
+console.log("inicio do programa");
+
+let nota1;
+let nota2;
+let nota3;
+
+nota1 = parseInt(prompt('digite a nota 1'));
+nota2 = parseInt(prompt('digite a nota 2'));
+nota3 = parseInt(prompt('digite a nota 3'));
+
+
+//calculo da média
+media=(nota1 + nota2 + nota3) / 3;
+
+
+console.log('a primeira nota do aluno é:'+ nota1);
+console.log('a segunda nota do aluno é:'+ nota2);
+console.log('a terceira nota do aluno é:'+ nota3);
+
+console.log('a média do aluno é:'+ media);
+
+console.log('fim do programa')
+
+
