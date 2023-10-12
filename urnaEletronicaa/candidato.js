f78050717ee83d2e42ec1098d83786baa3e78c08
24081986 <kesia.alves@ymail.com>
2023-10-12 16:59:20 -0300

    urna eletronica

--- urnaEletronicaa/candidato.js
+++ urnaEletronicaa/candidato.js
@@ -1,10 +1,11 @@
 export default class Candidato {
-    constructor(nome, legenda, numero,img) {
+    constructor(nome, legenda, numero,img) {   
         this.nome = nome
         this.legenda = legenda
         this.numero = numero
         this.img = img
     }
+     //** construtor, método executado no momento que a classe é instanciada, os atributos da classe são definidos dentro deste método. declarei o método construtor e entre parenteses definir a classe  ao ser instaciada, depois definir os atributos das classes passando para cada um os argumentos do método.**//
 
     get nomeCandidato() {
         return this.nome
@@ -17,4 +18,5 @@ export default class Candidato {
     get numeroCandidato() {
         return this.numero
     }
+    //*A sintaxe get vincula uma propriedade de objeto a uma função que será chamada quando essa propriedade for pesquisada.*//
 }
--- urnaEletronicaa/urnaController.js
+++ urnaEletronicaa/urnaController.js
@@ -1,26 +1,23 @@
 import Candidato from "../model/candidato.js"
 
-export default class Teclado {
-
+class urnaController {
     constructor() {
 
-        this.teclado = document.querySelectorAll(' .botao-numerico')
-        this.tecladoDeAcao = document.querySelectorAll(' .botao-acao')
-        this.primeiroNumero = document.querySelector(' .primeiro-numero')
-        this.segundoNumero = document.querySelector(' .segundo-numero')
-        this.telaEL = document.querySelector(' .tela')
-        this.visorEL = document.querySelector(' .visor')
-        this.visorCandidatoEL = document.querySelector(' .visorCandidato')
-        this.telaConfirmacao = document.querySelector(' .visor-confirmacao')
-        this.botaoBrancoEL = document.querySelector(' .botao-branco')
-        this.candidatoVotadoEL = document.querySelector(' .nome-candidato')
-        this.visorConfirmacaoCandidato = document.querySelector(' .visor-confirmacao-candidato')
-
-            this.candidatos [
-                new Candidato('Darth Vader' , 'SITH' , 8, '../../"./candidato-darth-vader.jpg"'),
-                new Candidato('spock' , '' , 14, '../.),
-                new Candidato('princesa Kitana' , '' , 21, '../
-            ]
+        this.criarCandidatos()
 
     }
+
+    criarCandidatos() {
+        let candidato = [
+            new Candidato('Darth Vader' , 'sith' , '8' , '../../../../imagem/candidato-darth-vader.jpg'),
+            new Candidato('Spock' , 'vulcano' , '14' , '../../../../imagem/candidato-spock.jpg'),
+            new Candidato('Seu Madruga' , 'Vila' , '71' , '../../../../imagem/candidato-seu-Madruga.jpg'),
+        ]
+
+        console.log(candidatos)
+       
+    }
 }
+
+window.App new urnaController()
+
